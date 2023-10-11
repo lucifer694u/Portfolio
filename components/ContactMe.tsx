97b34d7a296b13aaa3618e5636f2a7227892c4ae
himanshu <hk7620882@gmail.com>
2023-10-11 18:17:54 +0530

    contact

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -19,11 +19,11 @@ function ContactMe({}: Props) {
 
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl mb-10">
         Contact
       </h3>
 
-      <div className="flex flex-col space-y-10">
+      <div className="flex flex-col space-y-10 mt-20 md:mt-0">
         <h4 className="text-lg md:text-4xl font-semibold text-center">
           I have got just what you need.
           <span className="decoration-[#F7AB0A]/50 underline"> Lets Talk.</span>
